23b735ea642ef525d8a1f02ebb67bafb75df46f4
Vitalik_Zubik <leo_thebest@mail.ru>
2021-03-03 10:22:59 +0300

    config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
-module.export = {
+module.exports = {
    mode: 'development',
-   entry: './index.js',
+   entry: "./index.js",
    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist")
    }
-}
+};
